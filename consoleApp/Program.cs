356552aafab1cb85786c33ee1986f52daf8606b4
kemechial <kaanevran@gmail.com>
2024-10-04 16:02:10 +0300

    Initial commit for C# training.

---
+++ consoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+var Rate=4.6;
+Console.WriteLine("Hello, World!");
+Console.WriteLine(5000*Rate);
+
+var name = "John";
+var surname = "McConnell";
+
+var composite = name + surname;
+
+Console.WriteLine(composite);
